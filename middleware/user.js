3402dec8c44951deb66737cb2bcf93f9171e62f3
Unkn-o-wn <rememfor@gmail.com>
2020-11-01 13:09:48 +0300

    change middleware 2

--- middleware/user.js
+++ middleware/user.js
@@ -1,7 +1,7 @@
 const session = require('express-session');
 const User = require('../models/user.js');
 
-module.exports = async function (req,res,next) {w
+module.exports = async function (req,res,next) {
     res.setHeader("Content-Security-Policy", "script-src 'self' https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js")
     if (!req.session.user) {
         return next();
